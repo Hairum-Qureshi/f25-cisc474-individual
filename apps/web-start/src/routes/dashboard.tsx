cbc664619603c2971b56971f3dbc5ef4d72eee83
Hairum Qureshi <hqureshi@udel.edu>
2025-10-08 23:44:52 -0400

    finalized UI design for dashboard

--- apps/web-start/src/routes/dashboard.tsx
+++ apps/web-start/src/routes/dashboard.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useState } from 'react';
 import Course from '../components/Course';
+import Calendar from '../components/Calendar';
+import Assignment from '../components/Assignment';
+import Announcement from '../components/Announcement';
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
@@ -9,14 +12,14 @@ export const Route = createFileRoute('/dashboard')({
 function RouteComponent() {
   const [showGrades, setShowGrades] = useState(false);
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setShowGrades(e.target.checked);
   };
 
   return (
     <div className="bg-slate-300 h-screen">
       <div className="flex-1 p-3 flex space-x-4">
-        <div className="w-1/2 rounded-md bg-slate-200">
+        <div className="w-1/2 flex-5 rounded-md bg-slate-200">
           <div className="flex items-center">
             <h2 className="text-2xl font-semibold my-5 mx-5">My Courses</h2>
             <div className="ml-auto">
@@ -51,14 +54,55 @@ function RouteComponent() {
               grade={showGrades ? '100%' : ''}
             />
             <Course
-              courseName={'Intro Into Algorithms'}
+              courseName={'Intro Into Data Structures & Algorithms'}
               courseTimings={'T, R - 1:30 PM to 3:00 PM'}
               professorName={'Professor Alex'}
               grade={showGrades ? '89%' : ''}
             />
+            <Course
+              courseName={'English Literature'}
+              courseTimings={'T, R - 3:30 PM to 4:15 PM'}
+              professorName={'Professor Hue'}
+              grade={showGrades ? '92%' : ''}
+            />
+            <Course
+              courseName={'Intro Into LLMs'}
+              courseTimings={'M, T - 8:30 AM to 9:45 AM'}
+              professorName={'Professor Jane'}
+              grade={showGrades ? '79%' : ''}
+            />
           </div>
         </div>
-        <div className="border border-red-500 w-1/2 rounded-md"></div>
+        <div className="w-1/2 rounded-md bg-slate-200">
+          <div className="flex">
+            <div className="w-1/2 overflow-y-scroll">
+              <h2 className="text-2xl font-semibold my-5 mx-5">
+                Upcoming Deadlines
+              </h2>
+              <div className="space-y-5 mx-3 my-4 h-60 overflow-y-auto">
+                <Assignment />
+                <Assignment />
+                <Assignment />
+                <Assignment />
+                <Assignment />
+                <Assignment />
+                <Assignment />
+              </div>
+            </div>
+            <div className="w-1/2">
+              <Calendar />
+            </div>
+          </div>
+          <div className="w-full">
+            <h2 className="text-2xl font-semibold my-5 mx-5">
+              Recent Announcements
+            </h2>
+            <div className="space-y-5 mx-3 my-4 h-60 overflow-y-auto">
+              <Announcement />
+              <Announcement />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
