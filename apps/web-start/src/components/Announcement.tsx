f46ea931c10515241518ddfa494f159e791d1575
Hairum Qureshi <hqureshi@udel.edu>
2025-10-09 19:01:19 -0400

    added Module and ModuleSection component

---
+++ apps/web-start/src/components/Announcement.tsx
@@ -0,0 +1,16 @@
+export default function Announcement() {
+  return (
+    <div className="w-full flex p-3 bg-slate-100 rounded-md flex-col">
+      <h4 className="text-lg font-semibold">Announcement Title Here</h4>
+      <p className="text-sm font-semibold text-sky-700">
+        Course 1 | Posted by Professor X on 10/2/2025
+      </p>
+      <p className="text-base">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        veritatis, repellat enim ipsum a, minima alias suscipit excepturi
+        aliquam adipisci nostrum similique. Tempora officiis ad eligendi
+        provident aliquid vero magnam.
+      </p>
+    </div>
+  );
+}
---
+++ apps/web-start/src/components/ModuleSection.tsx
@@ -0,0 +1,5 @@
+export default function ModuleSection() {
+  return (
+    <div className="bg-slate-300 text-black p-2 rounded-sm">Module Section</div>
+  );
+}
