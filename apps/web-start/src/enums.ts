d4870098aeec15199ae73baf2c971af47d6cfa8a
Hairum Qureshi <hqureshi@udel.edu>
2025-10-10 00:30:53 -0400

    created enums file

---
+++ apps/web-start/src/enums.ts
@@ -0,0 +1,13 @@
+enum FileSystemType {
+  FOLDER = 'FOLDER',
+  FILE = 'FILE',
+}
+
+enum FileType {
+  TXT = 'TXT',
+  WORD = 'WORD',
+  PDF = 'PDF',
+  CODE = 'CODE',
+}
+
+export { FileSystemType, FileType };
