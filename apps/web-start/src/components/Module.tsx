5a4c86c7eb8eec918ed0634771ba5da66e56c04b
Hairum Qureshi <hqureshi@udel.edu>
2025-10-09 20:07:15 -0400

    tweaked colors

--- apps/web-start/src/components/Module.tsx
+++ apps/web-start/src/components/Module.tsx
@@ -12,12 +12,12 @@ export default function Module({ collapseAll }: ModuleProps) {
   const [openModule, setOpenModule] = useState(false);
 
   return (
-    <div className="w-full text-white text-xl font-semibold rounded-sm bg-slate-400 border-2 border-slate-400 mb-4">
+    <div className="w-full text-xl font-semibold rounded-sm bg-slate-300 border border-slate-400 mb-4">
       <div className="flex items-center justify-between rounded-sm px-3">
         <span className="py-3">Module</span>
         <button
           type="button"
-          className="bg-slate-500 p-1 rounded-sm hover:cursor-pointer"
+          className="bg-slate-500 p-1 rounded-sm hover:cursor-pointer text-white"
           onClick={() => setOpenModule(!openModule)}
           aria-label={openModule ? 'Collapse module' : 'Expand module'}
         >
