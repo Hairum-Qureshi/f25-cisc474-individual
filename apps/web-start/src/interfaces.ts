8f33ae7fda12c6ab1f08f8623518e9ac99702587
Hairum Qureshi <hqureshi@udel.edu>
2025-10-14 12:24:52 -0400

    added User interface and Role enum

--- apps/web-start/src/interfaces.ts
+++ apps/web-start/src/interfaces.ts
@@ -22,9 +22,30 @@ interface Course {
   updatedAt: string;
 }
 
+enum Role {
+  PROFESSOR = 'PROFESSOR',
+  TA = 'TA',
+  STUDENT = 'STUDENT',
+  ADMIN = 'ADMIN',
+}
+
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  fullName: string;
+  profilePicture: string;
+  bio: string | null;
+  role: Role;
+  createdAt: string;
+  updatedAt: string;
+  isAdmin: boolean;
+}
+
 interface NavbarProps {
   courseName?: string;
   courseID?: string;
+  user: User;
 }
 
 export type { FileSystemItemProps, CourseProps, Course, NavbarProps };
