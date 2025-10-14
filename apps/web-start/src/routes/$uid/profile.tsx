99dd4ea0789df12bcbd216c98edf1edb4b2a1f29
Hairum Qureshi <hqureshi@udel.edu>
2025-10-14 13:18:54 -0400

    replaced dummy placeholder data with cached data from React Query

--- apps/web-start/src/routes/$uid/profile.tsx
+++ apps/web-start/src/routes/$uid/profile.tsx
@@ -1,49 +1,46 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { FaEdit, FaRegUserCircle } from 'react-icons/fa';
+import { useQueryClient } from '@tanstack/react-query';
 import Course from '../../components/Course';
+import type { User } from '../../interfaces';
 
 export const Route = createFileRoute('/$uid/profile')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const queryClient = useQueryClient();
+  const userData: User = queryClient.getQueryData(['currUserData']) as User;
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-blue-50 flex justify-center py-10">
       <div className="w-full max-w-5xl grid grid-cols-3 gap-6">
         <div className="col-span-2 flex flex-col gap-6">
           <div className="bg-white shadow-sm rounded-xl p-6 flex items-center gap-4 border border-gray-100">
             <img
-              src="https://i.pinimg.com/474x/07/c4/72/07c4720d19a9e9edad9d0e939eca304a.jpg"
+              src={
+                userData.profilePicture ||
+                'https://i.pinimg.com/474x/07/c4/72/07c4720d19a9e9edad9d0e939eca304a.jpg'
+              }
               alt="Profile picture"
               className="w-30 h-30 rounded-full object-cover"
             />
             <div>
               <h2 className="text-3xl font-semibold text-gray-800">
-                Hairum Qureshi
+                {userData.fullName}
               </h2>
               <p className="text-sm text-gray-500">
-                Student ID: 123451251 | Major: Biology
+                Student ID: {userData.id} | Major: Biology
               </p>
             </div>
           </div>
           <div className="bg-white shadow-sm rounded-xl p-6 border border-gray-100">
             <h3 className="text-lg font-semibold text-gray-800 mb-4">
-              Hairum Qureshi's Bio:
+              {userData.fullName}'s Bio:
             </h3>
             <div className="space-y-4">
               <p className="text-slate-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                odit enim temporibus iusto rem eos laborum quae veniam suscipit
-                eius vero cum incidunt corrupti quibusdam eum libero, adipisci
-                nesciunt laudantium. Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Deserunt accusamus quia, aspernatur pariatur
-                itaque voluptatum earum dicta consequatur assumenda. Quisquam
-                maxime consequatur distinctio nisi facilis. Voluptate illum
-                tempora doloremque dignissimos! Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Voluptatibus sed quibusdam
-                adipisci iste. Officiis quibusdam mollitia nobis repellendus et
-                quidem sed facere, repudiandae modi, qui, reiciendis natus.
-                Amet, ad unde!
+                {userData.bio || 'No bio written'}
               </p>
             </div>
           </div>
