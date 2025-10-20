570c48181db91c84a2f68394cebc872ed2af091c
Hairum Qureshi <hqureshi@udel.edu>
2025-10-20 11:46:27 -0400

    updated 'getCourseById' method by populating ID fields to get detailed data

--- apps/api/src/course/course.service.ts
+++ apps/api/src/course/course.service.ts
@@ -10,6 +10,38 @@ export class CourseService {
   }
 
   async getCourseById(id: string) {
-    return this.prisma.course.findUnique({ where: { id } });
+    // used ChatGPT to understand how to use Prisma to populate the ID references to get more data
+    return this.prisma.course.findUnique({
+      where: { id },
+      include: {
+        professor: {
+          select: {
+            id: true,
+            fullName: true,
+            email: true,
+          },
+        },
+        tas: {
+          select: {
+            id: true,
+            fullName: true,
+          },
+        },
+        students: {
+          select: {
+            id: true,
+            fullName: true,
+          },
+        },
+        assignments: {
+          select: {
+            id: true,
+            title: true,
+            dueDate: true,
+            module: true,
+          },
+        },
+      },
+    });
   }
 }
