f5050070b1e2398f20b6ed4c68004ecfd9104ba3
Hairum Qureshi <hqureshi@udel.edu>
2025-10-21 15:23:15 -0400

    added data retrieval for course by ID from React Query cache

--- apps/web-start/src/routes/course/$courseID/index.tsx
+++ apps/web-start/src/routes/course/$courseID/index.tsx
@@ -2,18 +2,35 @@
 import { createFileRoute } from '@tanstack/react-router';
 import Assignment from '../../../components/Assignment';
 import Announcement from '../../../components/Announcement';
+import { useQuery } from '@tanstack/react-query';
 
 export const Route = createFileRoute('/course/$courseID/')({
   component: CourseOverviewPage,
 });
 
 function CourseOverviewPage() {
+  const { courseID } = Route.useParams();
+
+  const { data, isLoading } = useQuery({
+    queryKey: [`course ${courseID}`],
+    queryFn: ({ queryKey }) => {
+      const keyString = queryKey[0];
+      const courseId = keyString?.split(' ')[1];
+
+      return fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/courses/${courseId}`,
+      ).then((res) => res.json());
+    },
+  });
+
   return (
     <div className="bg-slate-300 h-screen">
       <div className="flex-1 p-3 flex space-x-4">
         <div className="w-1/2 flex-5 rounded-md bg-slate-200">
           <div className="flex items-center">
-            <h2 className="text-2xl font-semibold my-5 mx-5">Course 1</h2>
+            <h2 className="text-2xl font-semibold my-5 mx-5">
+              {isLoading ? 'Loading name...' : data?.courseName}
+            </h2>
             <div className="ml-auto"></div>
           </div>
           <div className="space-y-5 mx-3 h-3/4 my-2 overflow-y-scroll">
