17a65f5f1003abfa1316791bd4236f2b56baee9e
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 16:22:16 -0400

    removed Folder module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,6 @@ import { LinksModule } from './links/links.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
-import { FolderModule } from './folder/folder.module';
 import { FileModule } from './file/file.module';
 import { CourseModule } from './course/course.module';
 import { CourseGradeModule } from './course_grade/course_grade.module';
@@ -16,7 +15,6 @@ import { SubmissionModule } from './submission/submission.module';
     LinksModule,
     UserModule,
     SubmissionModule,
-    FolderModule,
     FileModule,
     CourseModule,
     CourseGradeModule,
