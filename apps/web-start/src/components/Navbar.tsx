5d3d2d4775c62f6b97317dd8fcd8740406c636bf
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 12:46:36 -0400

    replaced hardcoded data with dynamic temp curr user data

--- apps/web-start/src/components/Navbar.tsx
+++ apps/web-start/src/components/Navbar.tsx
@@ -3,11 +3,21 @@ import { useState } from 'react';
 import { IoSunny } from 'react-icons/io5';
 import { FaMoon } from 'react-icons/fa';
 import type { NavbarProps } from '../interfaces';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Navbar({ courseName, courseID }: NavbarProps) {
   const [lightMode, setLightMode] = useState(true);
 
   const CURR_UID = 'cmh2ol4jb001ksb0rhr07fa3j';
+
+  const { data: currUserData } = useQuery({
+    queryKey: ['currUserData'],
+    queryFn: () => {
+      return fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/users/${CURR_UID}`,
+      ).then((res) => res.json());
+    },
+  });
 
   const routerState = useRouterState();
 
@@ -17,17 +27,19 @@ export default function Navbar({ courseName, courseID }: NavbarProps) {
   return (
     <nav className="w-full bg-slate-200 p-4 h-20">
       <div className="flex items-center">
-        <div className="w-12 h-12 rounded-md border-2 border-slate-400">
+        <div className="w-12 h-12 rounded-lg border-2 border-slate-400">
           <img
-            src="https://i.pinimg.com/474x/07/c4/72/07c4720d19a9e9edad9d0e939eca304a.jpg"
+            src={currUserData?.profilePicture}
             alt="User profile picture"
             className="w-full h-full rounded-md object-cover"
           />
         </div>
         <div className="flex flex-col">
-          <h3 className="font-semibold ml-3 text-lg">Hairum Qureshi</h3>
+          <h3 className="font-semibold ml-3 text-lg">
+            {currUserData?.fullName}
+          </h3>
           <h3 className="font-semibold ml-3 text-slate-600 text-sm">
-            UID: 123451251
+            UID: {currUserData?.id}
           </h3>
         </div>
         <div className="ml-auto flex items-center space-x-5">
