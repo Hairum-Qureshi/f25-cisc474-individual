d85aad69e17cf7191884e52ac126c52a4c3c3557
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 19:37:27 -0400

    wrapped Link around section div so users can now go to that specific assignment details page

--- apps/web-start/src/components/ModuleSection.tsx
+++ apps/web-start/src/components/ModuleSection.tsx
@@ -1,11 +1,27 @@
+import { Link, useRouterState } from '@tanstack/react-router';
+
 interface ModuleSectionProps {
   title: string;
+  assignmentID: string;
 }
 
-export default function ModuleSection({ title }: ModuleSectionProps) {
+export default function ModuleSection({
+  title,
+  assignmentID,
+}: ModuleSectionProps) {
+  const routerState = useRouterState();
+  const currentMatch = routerState.matches.at(-1);
+  const params = currentMatch?.params as { courseID?: string } | undefined;
+  const courseID = params?.courseID;
+
   return (
-    <div className="bg-slate-400 text-white p-3 rounded-sm">
-      <h3 className="font-semibold">{title}</h3>
-    </div>
+    <Link
+      to="/course/$courseID/$assignmentID/assignment"
+      params={{ courseID: courseID!, assignmentID }}
+    >
+      <div className="bg-slate-400 text-white p-3 rounded-sm mb-3">
+        <h3 className="font-semibold">{title}</h3>
+      </div>
+    </Link>
   );
 }
