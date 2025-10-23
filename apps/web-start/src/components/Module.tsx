c9ee39e7cee9dd7c9dbb13e258ce6e597f812a9a
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 17:40:40 -0400

    replaced hardcoded data with dynamic data to render ModuleSections dynamically

--- apps/web-start/src/components/Module.tsx
+++ apps/web-start/src/components/Module.tsx
@@ -4,17 +4,23 @@ import ModuleSection from './ModuleSection';
 
 interface ModuleProps {
   collapseAll: boolean;
+  module: string;
+  moduleMetaData?: any[];
 }
 
 // ! CollapseAll logic needs to be fixed
 
-export default function Module({ collapseAll }: ModuleProps) {
+export default function Module({
+  collapseAll,
+  module,
+  moduleMetaData,
+}: ModuleProps) {
   const [openModule, setOpenModule] = useState(false);
 
   return (
     <div className="w-full text-xl font-semibold rounded-sm bg-slate-300 border border-slate-400 mb-4">
       <div className="flex items-center justify-between rounded-sm px-3">
-        <span className="py-3">Module</span>
+        <span className="py-3">{module}</span>
         <button
           type="button"
           className="bg-slate-500 p-1 rounded-sm hover:cursor-pointer text-white"
@@ -28,11 +34,12 @@ export default function Module({ collapseAll }: ModuleProps) {
       {openModule ||
         (collapseAll && (
           <div className="space-y-2 mx-3 mb-3">
-            <ModuleSection />
-            <ModuleSection />
-            <ModuleSection />
-            <ModuleSection />
-            <ModuleSection />
+            {moduleMetaData?.map((section: any) => (
+              <ModuleSection
+                key={section.id}
+                title={section.title}
+              />
+            ))}
           </div>
         ))}
     </div>
