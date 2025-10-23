379a6860314d36cfe94618b7aebcdd2b93d34733
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 17:40:06 -0400

    replaced hardcoded data with dynamic title prop

--- apps/web-start/src/components/ModuleSection.tsx
+++ apps/web-start/src/components/ModuleSection.tsx
@@ -1,5 +1,11 @@
-export default function ModuleSection() {
+interface ModuleSectionProps {
+  title: string;
+}
+
+export default function ModuleSection({ title }: ModuleSectionProps) {
   return (
-    <div className="bg-slate-400 text-white p-3 rounded-sm">Module Section</div>
+    <div className="bg-slate-400 text-white p-3 rounded-sm">
+      <h3 className="font-semibold">{title}</h3>
+    </div>
   );
 }
