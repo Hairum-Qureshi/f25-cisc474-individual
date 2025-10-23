3e575c5939244fb1d9ea776db4e03c4ac784573c
Hairum Qureshi <hqureshi@udel.edu>
2025-10-23 18:22:10 -0400

    finalized content rendering logic

--- apps/web-start/src/components/Announcement.tsx
+++ apps/web-start/src/components/Announcement.tsx
@@ -1,16 +1,20 @@
-export default function Announcement() {
+import type { AnnouncementProps } from '../interfaces';
+
+export default function Announcement({
+  title,
+  poster,
+  content,
+  date,
+}: AnnouncementProps) {
   return (
     <div className="w-full flex p-3 bg-slate-100 rounded-md flex-col">
-      <h4 className="text-lg font-semibold">Announcement Title Here</h4>
+      <h4 className="text-lg font-semibold">{title}</h4>
       <p className="text-sm font-semibold text-sky-700">
-        Course 1 | Posted by Professor X on 10/2/2025
+        {!poster
+          ? `Posted on ${new Date(date).toLocaleDateString()}`
+          : `Posted by ${poster} on ${new Date(date).toLocaleDateString()}`}
       </p>
-      <p className="text-base">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-        veritatis, repellat enim ipsum a, minima alias suscipit excepturi
-        aliquam adipisci nostrum similique. Tempora officiis ad eligendi
-        provident aliquid vero magnam.
-      </p>
+      <p className="text-base">{content}</p>
     </div>
   );
 }
