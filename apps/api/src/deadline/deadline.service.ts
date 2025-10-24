dc95fc6a17a94bdb2f311ff269d2d20a7bdbb328
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 02:23:29 -0400

    updated imports for 'DeadlineCreateIn' and 'DeadlineUpdateIn' imports

--- apps/api/src/deadline/deadline.service.ts
+++ apps/api/src/deadline/deadline.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { DeadlineCreateIn, DeadlineUpdateIn } from 'src/deadlines.dto';
+import { DeadlineCreateIn, DeadlineUpdateIn } from '@repo/api/deadlines';
 
 @Injectable()
 export class DeadlineService {
