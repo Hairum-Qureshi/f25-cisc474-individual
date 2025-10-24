df16537f243607762245102a2ff6a06e2fc3d015
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 11:50:00 -0400

    added Auth0Provider context and wrapped it around Tanstack app with Auth0 config

--- apps/web-start/src/router.tsx
+++ apps/web-start/src/router.tsx
@@ -4,10 +4,16 @@ import * as TanstackQuery from './integrations/root-provider';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 // Create a new router instance
 export const getRouter = () => {
   const rqContext = TanstackQuery.getContext();
+
+  const redirect_uri =
+    typeof window !== 'undefined'
+      ? window.location.origin + '/dashboard'
+      : undefined;
 
   const router = createRouter({
     routeTree,
@@ -15,9 +21,17 @@ export const getRouter = () => {
     defaultPreload: 'intent',
     Wrap: (props: { children: React.ReactNode }) => {
       return (
-        <TanstackQuery.Provider {...rqContext}>
-          {props.children}
-        </TanstackQuery.Provider>
+        <Auth0Provider
+          domain={import.meta.env.VITE_AUTH0_DOMAIN}
+          clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+          authorizationParams={{
+            redirect_uri: redirect_uri,
+          }}
+        >
+          <TanstackQuery.Provider {...rqContext}>
+            {props.children}
+          </TanstackQuery.Provider>
+        </Auth0Provider>
       );
     },
   });
