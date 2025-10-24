30fd841e96e2fad88ad4b6db8709645d7e2caa2b
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 01:56:44 -0400

    added CRUD methods that call service methods

--- apps/api/src/deadline/deadline.controller.ts
+++ apps/api/src/deadline/deadline.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { DeadlineService } from './deadline.service';
+import { DeadlineCreateIn, DeadlineUpdateIn } from 'src/deadlines.dto';
 
 @Controller('deadline')
 export class DeadlineController {
   constructor(private readonly deadlineService: DeadlineService) {}
+
+  @Get()
+  async getAllDeadlines() {
+    return this.deadlineService.getAll();
+  }
+
+  @Get(':id')
+  async getDeadlineById(@Param('id') id: string) {
+    return this.deadlineService.getById(id);
+  }
+
+  @Post()
+  createDeadline(@Body() createCourseDto: DeadlineCreateIn) {
+    return this.deadlineService.create(createCourseDto);
+  }
+
+  @Patch()
+  async updateDeadline(
+    @Param('id') id: string,
+    @Body() updateDeadlineDto: DeadlineUpdateIn,
+  ) {
+    return this.deadlineService.update(id, updateDeadlineDto);
+  }
+
+  @Delete()
+  async deleteDeadline(@Param('id') id: string) {
+    return this.deadlineService.delete(id);
+  }
 }
