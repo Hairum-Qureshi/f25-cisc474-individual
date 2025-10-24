15d98a6eeb13a983254cb87bd66ddcf8fe95b027
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 10:25:13 -0400

    added authentication route guards to all CRUD endpoints

--- apps/api/src/deadline/deadline.controller.ts
+++ apps/api/src/deadline/deadline.controller.ts
@@ -6,30 +6,36 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { DeadlineService } from './deadline.service';
 import { DeadlineCreateIn, DeadlineUpdateIn } from '@repo/api/deadlines';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('deadlines')
 export class DeadlineController {
   constructor(private readonly deadlineService: DeadlineService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async getAllDeadlines() {
     return this.deadlineService.getAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async getDeadlineById(@Param('id') id: string) {
     return this.deadlineService.getById(id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   createDeadline(@Body() createCourseDto: DeadlineCreateIn) {
     return this.deadlineService.create(createCourseDto);
   }
 
-  @Patch(":id")
+  @UseGuards(AuthGuard('jwt'))
+  @Patch(':id')
   async updateDeadline(
     @Param('id') id: string,
     @Body() updateDeadlineDto: DeadlineUpdateIn,
@@ -37,6 +43,7 @@ export class DeadlineController {
     return this.deadlineService.update(id, updateDeadlineDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   async deleteDeadline(@Param('id') id: string) {
     return this.deadlineService.delete(id);
