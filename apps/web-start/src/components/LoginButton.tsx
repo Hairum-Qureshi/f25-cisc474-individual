6fb994277203c5c85e76240a2cce9156cf09f6ad
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 11:51:07 -0400

    added LoginButton component

---
+++ apps/web-start/src/components/LoginButton.tsx
@@ -0,0 +1,23 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button
+      onClick={() =>
+        loginWithRedirect({
+          authorizationParams: {
+            scope: 'read:courses',
+            prompt: 'consent',
+          },
+        })
+      }
+      className="bg-green-500 p-2 m-10 text-white rounded-md hover:bg-green-600 transition-colors hover:cursor-pointer"
+    >
+      Log In
+    </button>
+  );
+};
+
+export default LoginButton;
