e3ed484fb4b733922cdfeaa34fef801ceddc208b
Hairum Qureshi <hqureshi@udel.edu>
2025-10-24 10:21:46 -0400

    added authentication guards to routes and added a findAll method for the current user

--- apps/api/src/course/course.controller.ts
+++ apps/api/src/course/course.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/auth/current-user-decorator';
+import { JwtUser } from 'src/auth/jwt.strategy';
 
 @Controller('courses')
 export class CourseController {
   constructor(private courseService: CourseService) {}
 
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  findAll(@CurrentUser() user: JwtUser) {
+    console.log('User accessed:', user);
+    return this.courseService.getAllCourses();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async getAllCourses() {
     return this.courseService.getAllCourses();
