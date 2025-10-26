e4f153eb8b00f692c4e0ca93556a4c78a7bca427
Hairum Qureshi <hqureshi@udel.edu>
2025-10-26 14:08:15 -0400

    removed 'settings' page

--- apps/web-start/src/routes/$uid/profile/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/$uid/profile/settings')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/$uid/profile/settings"!</div>
-}
