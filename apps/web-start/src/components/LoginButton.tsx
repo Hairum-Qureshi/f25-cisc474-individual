16c5beeee06fe2a0140c45809c51b6a382bd7977
Hairum Qureshi <hqureshi@udel.edu>
2025-10-26 11:17:57 -0400

    modified TailwindCSS styling for button

--- apps/web-start/src/components/LoginButton.tsx
+++ apps/web-start/src/components/LoginButton.tsx
@@ -14,7 +14,7 @@ const LoginButton = () => {
           },
         })
       }
-      className="bg-green-500 p-2 m-10 text-white rounded-md hover:bg-green-600 transition-colors hover:cursor-pointer"
+      className="bg-green-500 p-2 w-full text-white rounded-md hover:bg-green-600 transition-colors hover:cursor-pointer"
     >
       Log In
     </button>
